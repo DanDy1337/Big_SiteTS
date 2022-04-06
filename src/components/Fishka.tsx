be19797ff1b9c15b09423f6fb859f981bd074552
DanDy1337 <jackpadington1@gmail.com>
2022-04-06 22:54:47 +0300

    Исправлено зависание при движении избражений x4

--- src/components/Fishka.tsx
+++ src/components/Fishka.tsx
@@ -27,9 +27,9 @@ function Fishka(){
 
             pos2 = e.clientY - startCoordOfMouse[0] + 200
 
-            img1.style.transform = 'translate(' + pos1h/10 + 'px' + ', 0' + ') rotate(' + pos1/14 + 'deg' + ') scale(-1,1) '
+            img1.style.transform = 'translate(' + '-' + pos1h/16 + 'px' + ', 0' + ') rotate(' + pos1/14 + 'deg' + ') scale(-1,1) '
 
-            img2.style.transform = 'translate(' + pos1h/10 + 'px' + ', 0' + ') rotate(' + pos2/14 + 'deg' + ')  '
+            img2.style.transform = 'translate(' + pos1h/16 + 'px' + ', 0' + ') rotate(' + pos2/14 + 'deg' + ')  '
 
         }
 
